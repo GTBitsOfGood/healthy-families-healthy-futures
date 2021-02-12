db60ccef50db341d114b5833ee951a10ff344eba
ishan baniya <ibaniya98@gmail.com>
2021-02-12 17:39:53 -0500

    Create route for donations page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,6 +14,9 @@ function Navigation(): JSX.Element {
         <li className={styles.navigationItem}>
           <Link to="/blog/">Blog</Link>
         </li>
+        <li className={styles.navigationItem}>
+          <Link to="/donations/">Donate</Link>
+        </li>
       </ul>
     </nav>
   );
---
+++ src/pages/donations.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Container, Heading } from '@chakra-ui/react';
+import { graphql, PageProps } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+import Layout from '../components/Layout';
+
+interface Props extends PageProps {
+  data: GatsbyTypes.DonationQueryQuery;
+}
+
+function DonationIndex(props: Props): JSX.Element {
+  const siteTitle = props.data.site?.siteMetadata?.title || '';
+  return (
+    <Layout location={props.location}>
+      <Container alignItems="center">
+        <Helmet title={`Donations | ${siteTitle}`} />
+        <Heading as="h1" size="3xl">
+          Donations
+        </Heading>
+      </Container>
+    </Layout>
+  );
+}
+
+export default DonationIndex;
+
+export const pageQuery = graphql`
+  query DonationQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
