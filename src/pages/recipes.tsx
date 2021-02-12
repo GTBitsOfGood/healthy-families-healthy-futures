153107e52e1b8afd23c6049e959ac351d1a9d738
Edmund Xin <mxinburritos@gmail.com>
2021-02-12 11:04:14 -0800

    feat: add recipe route

---
+++ src/pages/recipes.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { Divider } from '@chakra-ui/react';
+import { graphql, PageProps } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import Layout from 'src/components/Layout';
+
+import styles from '../css/Blog.module.css';
+
+interface Props extends PageProps {
+  data: GatsbyTypes.RecipeQueryQuery;
+}
+
+function RecipesIndex(props: Props): JSX.Element {
+  const siteTitle = props.data.site?.siteMetadata?.title;
+  const posts = props.data?.allContentfulRecipe?.edges;
+
+  return (
+    <Layout location={props.location}>
+      <div style={{ background: '#fff' }}>
+        <Helmet title={siteTitle} />
+        <div className={styles.hero}>Recipes</div>
+        <div className="wrapper">
+          <h2 className="section-headline">Wonderful Recipes</h2>
+          <Divider />
+          <ul className="article-list">
+            {posts.map(({ node }) => {
+              return (
+                <li key={node.id}>
+                  <h1>Hello</h1>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default RecipesIndex;
+
+export const pageQuery = graphql`
+  query RecipeQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulRecipe {
+      edges {
+        node {
+          id
+          prepTime
+          title
+          totalTime
+          updatedAt
+        }
+      }
+    }
+  }
+`;
