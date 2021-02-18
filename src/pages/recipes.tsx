2fe5fd70f1b016024b2b289fea253b36fdc3957f
Edmund Xin <mxinburritos@gmail.com>
2021-02-17 18:20:32 -0800

    fix: rename posts to recipes

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Divider } from '@chakra-ui/react';
-import { graphql, PageProps } from 'gatsby';
+import { graphql, Link, PageProps } from 'gatsby';
 import { Helmet } from 'react-helmet';
 import Layout from 'src/components/Layout';
 
@@ -13,7 +13,7 @@ interface Props extends PageProps {
 
 function RecipesIndex(props: Props): JSX.Element {
   const siteTitle = props.data.site?.siteMetadata?.title;
-  const posts = props.data?.allContentfulRecipe?.edges;
+  const recipes = props.data?.allContentfulRecipe?.nodes;
 
   return (
     <Layout location={props.location}>
@@ -24,10 +24,12 @@ function RecipesIndex(props: Props): JSX.Element {
           <h2 className="section-headline">Wonderful Recipes</h2>
           <Divider />
           <ul className="article-list">
-            {posts.map(({ node }) => {
+            {recipes.map(node => {
               return (
                 <li key={node.id}>
-                  <h1>{node.title}</h1>{' '}
+                  <Link to={`/recipes/${node.title ?? ''}`}>
+                    <h1>{node.title}</h1>{' '}
+                  </Link>
                   <h1>{`${String(node.prepTime)} ${node.prepTime === 1 ? 'min' : 'mins'}`}</h1>
                   <h1>{`${String(node.totalTime)} ${node.totalTime === 1 ? 'min' : 'mins'}`}</h1>
                 </li>
@@ -50,14 +52,12 @@ export const pageQuery = graphql`
       }
     }
     allContentfulRecipe {
-      edges {
-        node {
-          id
-          prepTime
-          title
-          totalTime
-          updatedAt
-        }
+      nodes {
+        id
+        prepTime
+        title
+        totalTime
+        updatedAt
       }
     }
   }
