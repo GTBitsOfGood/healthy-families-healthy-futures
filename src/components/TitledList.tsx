1aff9741ab9c6f78070de1fec2a95a4de5d57c81
Matthew Bernet <mattbernetGT@gmail.com>
2021-02-24 07:10:35 -0500

    Full recipe page, some data issues to resolve still

---
+++ src/components/TitledList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Text, HStack, VStack, Divider, OrderedList, ListItem, Box } from '@chakra-ui/react';
+
+interface Props {
+  title: string;
+  listElements: string[] | undefined;
+}
+
+function TitledList({ title, listElements }: Props): JSX.Element {
+  return (
+    <VStack align="stretch" spacing={4}>
+      <HStack align="center" spacing={4}>
+        <Text fontWeight="bold" fontSize="xl">
+          {title}
+        </Text>
+        <Divider width="full" borderColor="green.400" />
+      </HStack>
+      <Box>
+        <OrderedList listStylePos="outside" color="green.400" spacing={2}>
+          {listElements?.map(element => (
+            <ListItem key={element}>
+              <Box marginLeft={5}>
+                <Text as="span" color="black">
+                  {element}
+                </Text>
+              </Box>
+            </ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </VStack>
+  );
+}
+
+export default TitledList;
--- src/pages/recipes/{ContentfulRecipe.title}.tsx
+++ src/pages/recipes/{ContentfulRecipe.title}.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { ChevronLeftIcon } from '@chakra-ui/icons';
 import { Flex, Heading, IconButton, Text, Image, HStack, VStack, Box } from '@chakra-ui/react';
-import { graphql, PageProps } from 'gatsby';
+import { graphql, Link, PageProps } from 'gatsby';
+import TitledList from 'src/components/TitledList';
 
 import Layout from '../../components/Layout';
 
@@ -12,63 +13,83 @@ interface Props extends PageProps {
 
 function RecipeTemplate(props: Props): JSX.Element {
   const recipe = props.data.contentfulRecipe;
+  const ingredients = recipe?.ingredients?.ingredients?.split('-').filter(x => x);
+  const prepDirections = recipe?.prepDirections?.prepDirections?.split('-').filter(x => x);
+  const instructions = recipe?.directions?.directions?.split('-').filter(x => x);
+  const notes = recipe?.notes?.notes?.split('-').filter(x => x);
 
-  //TODO: Delete this, just a placeholder for now
-  const ingredients = ['Ingredient 1', 'Ingredient 2', 'Ingredient 3', 'Ingredient 4'];
+  // How do I know when to consider ingredient optional information when I am given a string?
+  const ingredientPairs = ingredients?.reduce((pairs, _, index, ingredients) => {
+    if (index % 2 == 0) {
+      pairs.push(ingredients.slice(index, index + 2));
+    }
+
+    return pairs;
+  }, []);
+
+  // Need to discuss how we are determining if browser is in mobile view
+  const isMobile = false;
 
   return (
     <Layout location={props.location}>
-      <Box>
-        <HStack align="center" spacing={2}>
-          <IconButton
-            aria-label="Back button"
-            icon={<ChevronLeftIcon w={7} h={7} />}
-            size="sm"
-            backgroundColor="white"
-          ></IconButton>
-          <Text>Back to Recipes</Text>
-        </HStack>
-        <Flex justify="space-between" marginTop={5}>
-          <VStack align="start" spacing={5}>
-            <Heading>{recipe?.title}</Heading>
-            <HStack align="start" spacing={10}>
-              <VStack align="start">
-                <Text fontWeight="bold">Total Time:</Text>
-                <Text fontWeight="bold">Prep Time:</Text>
-                <Text fontWeight="bold">Ingredients:</Text>
-              </VStack>
-              <VStack align="start">
-                <Text>{recipe?.totalTime} min</Text>
-                <Text>{recipe?.prepTime} min</Text>
-                <VStack>
-                  {ingredients.map(ingredient => (
-                    <Text key={ingredient}>{ingredient}</Text>
-                  ))}
+      {isMobile ? (
+        <Box>Mobile Layout Here</Box>
+      ) : (
+        <Box marginY={10}>
+          <HStack align="center" spacing={2}>
+            <Link to="/recipes">
+              <IconButton
+                aria-label="Back button"
+                icon={<ChevronLeftIcon w={7} h={7} />}
+                size="sm"
+                backgroundColor="white"
+              ></IconButton>
+            </Link>
+            <Text>Back to Recipes</Text>
+          </HStack>
+          <Flex justify="space-between" marginTop={5}>
+            <VStack align="start" spacing={5}>
+              <Heading>{recipe?.title}</Heading>
+              <HStack align="start" spacing={10}>
+                <VStack align="start">
+                  <Text fontWeight="bold">Prep Time</Text>
+                  <Text fontWeight="bold">Total Time</Text>
+                  <Text fontWeight="bold">Yield</Text>
+                  <Text fontWeight="bold">Ingredients</Text>
                 </VStack>
-              </VStack>
-            </HStack>
-          </VStack>
-          <Image
-            src=""
-            fallbackSrc="https://via.placeholder.com/150"
-            alt="Recipe Image"
-            boxSize="sm"
-            objectFit="cover"
-          ></Image>
-        </Flex>
-        <VStack align="start">
-          <Text fontWeight="bold" fontSize="xl">
-            Prep
-          </Text>
-          <Text>{recipe?.prepDirections?.prepDirections}</Text>
-        </VStack>
-        <VStack align="start">
-          <Text fontWeight="bold" fontSize="xl">
-            Instructions
-          </Text>
-          <Text>{recipe?.directions?.directions}</Text>
-        </VStack>
-      </Box>
+                <VStack align="start">
+                  <Text>{recipe?.prepTime} min</Text>
+                  <Text>{recipe?.totalTime} min</Text>
+                  <Text>{recipe?.yield}</Text>
+                  <VStack align="start">
+                    {ingredientPairs?.map(pair => (
+                      <HStack key={pair[0]}>
+                        <Text>{pair[0]}</Text>
+                      </HStack>
+                    ))}
+                  </VStack>
+                </VStack>
+              </HStack>
+            </VStack>
+            <Image
+              src=""
+              fallbackSrc="https://via.placeholder.com/150"
+              alt="Recipe Image"
+              boxSize="sm"
+              objectFit="cover"
+            ></Image>
+          </Flex>
+          <TitledList title="Prep" listElements={prepDirections}></TitledList>
+          <br></br>
+          <TitledList title="Instructions" listElements={instructions}></TitledList>
+          {notes !== undefined && notes.length > 0 && (
+            <Box>
+              <br></br>
+              <TitledList title="Notes" listElements={notes}></TitledList>
+            </Box>
+          )}
+        </Box>
+      )}
     </Layout>
   );
 }
@@ -81,6 +102,7 @@ export const pageQuery = graphql`
       title
       totalTime
       prepTime
+      yield
       ingredients {
         ingredients
       }
@@ -89,6 +111,9 @@ export const pageQuery = graphql`
       }
       prepDirections {
         prepDirections
+      }
+      notes {
+        notes
       }
     }
   }
