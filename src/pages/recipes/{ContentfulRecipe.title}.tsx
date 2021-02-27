29a745ed9b1ee263aa8adbe404153cea54446456
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2021-02-27 00:59:38 -0500

    added mobile

--- src/pages/recipes/{ContentfulRecipe.title}.tsx
+++ src/pages/recipes/{ContentfulRecipe.title}.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 import { ChevronLeftIcon } from '@chakra-ui/icons';
 import { Flex, Heading, IconButton, Text, Image, HStack, VStack, Box } from '@chakra-ui/react';
@@ -6,17 +7,26 @@ import { graphql, Link, PageProps } from 'gatsby';
 import TitledList from 'src/components/TitledList';
 
 import Layout from '../../components/Layout';
+import { truncate } from 'fs/promises';
 
 interface Props extends PageProps {
   data: GatsbyTypes.RecipeByTitleQuery;
 }
 
+
+const CenterImage = styled(Image) `width: 100%`;
+const Container = styled(Box) `padding: 0px 16px 20px 16px`;  
+const Ingred = styled(Text) `padding: 30px 0px 10px 0px; font-size: 18px`;
+const Minutes = styled(Text) `padding: 15px 0px 0px 0px; font-size: 16px`;
+const Title = styled(Heading) `font-size: 18px`;
+const Preparation = styled(Text) `padding: 15px 0px 0px 0px`
 function RecipeTemplate(props: Props): JSX.Element {
   const recipe = props.data.contentfulRecipe;
   const ingredients = recipe?.ingredients?.ingredients?.split('-').filter(x => x);
   const prepDirections = recipe?.prepDirections?.prepDirections?.split('-').filter(x => x);
   const instructions = recipe?.directions?.directions?.split('-').filter(x => x);
   const notes = recipe?.notes?.notes?.split('-').filter(x => x);
+  console.log(props.data.contentfulRecipe);
 
   // How do I know when to consider ingredient optional information when I am given a string?
   const ingredientPairs = ingredients?.reduce((pairs, _, index, ingredients) => {
@@ -33,7 +43,47 @@ function RecipeTemplate(props: Props): JSX.Element {
   return (
     <Layout location={props.location}>
       {isMobile ? (
-        <Box>Mobile Layout Here</Box>
+        <Container>
+           <Title align = "center">{recipe?.title}</Title> 
+           <CenterImage 
+              src=""
+              fallbackSrc="https://via.placeholder.com/150"
+              alt="Recipe Image"
+              boxSize="sm"
+            ></CenterImage>
+            <HStack align="start" spacing={10}>
+            <VStack align="start">
+                  <Minutes fontWeight="bold">Prep Time  </Minutes>
+                  <Text fontWeight="bold">Total Time </Text>
+                  <Text fontWeight="bold">Servings   </Text>
+            </VStack>
+            <VStack align="start">
+                  <Minutes>{recipe?.prepTime} min</Minutes>
+                  <Text>{recipe?.totalTime} min</Text>
+                  <Text>{recipe?.yield}</Text>
+            </VStack>
+            </HStack>
+            <Ingred fontWeight="bold">Ingredients</Ingred>
+            <VStack align="start">
+                    {ingredientPairs?.map(pair => (
+                      <HStack key={pair[0]}>
+                        <Text>{pair[0]}</Text>
+                      </HStack>
+                    ))}
+            </VStack>
+            <Preparation>
+            <TitledList title="Prep" listElements={prepDirections}></TitledList>
+            </Preparation>
+          <br></br>
+          <TitledList title="Instructions" listElements={instructions}></TitledList>
+          {notes !== undefined && notes.length > 0 && (
+            <Box>
+              <br></br>
+              <TitledList title="Notes" listElements={notes}></TitledList>
+            </Box>
+          )}
+        </Container>
+
       ) : (
         <Box marginY={10}>
           <HStack align="center" spacing={2}>
