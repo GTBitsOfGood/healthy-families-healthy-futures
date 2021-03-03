a7573589f6b2fc42a13a89b3a6b7781d516665f8
mbclr3 <mbclr3@gatech.edu>
2021-03-02 20:50:27 -0500

    Add a logo from Contentful

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import { Link } from 'gatsby';
+import { Box, Button } from '@chakra-ui/react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 
 import styles from '../css/Header.module.css';
-// import logo from '../img/logo.png';
 
 function Header(): JSX.Element {
+  const data: GatsbyTypes.HeaderQueryQuery = useStaticQuery<GatsbyTypes.HeaderQueryQuery>(graphql`
+    query HeaderQuery {
+      contentfulAsset(title: { eq: "Logo" }) {
+        fluid(quality: 100) {
+          ...GatsbyContentfulFluid
+        }
+        description
+      }
+    }
+  `);
+
   return (
     <div className={styles.navigation}>
-      {/* <img className={styles.logo} src={logo} alt="Healthy Families Healthy Futures Logo" /> */}
+      <Box w="150px">
+        {data.contentfulAsset?.fluid != null ? (
+          <Img fluid={data.contentfulAsset.fluid} alt={data.contentfulAsset.description} />
+        ) : null}
+      </Box>
       <input
         className={styles.search}
         type="text"
@@ -19,9 +33,7 @@ function Header(): JSX.Element {
         name="s"
       />
       <p className={styles.getInvolved}>GET INVOLVED</p>
-      <Button className={styles.donateButton} colorScheme="green">
-        DONATE
-      </Button>
+      <Button variant="primary">Donate</Button>
     </div>
   );
 }
