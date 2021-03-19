aafa46556288918006bd163e0f40848447bcccd2
Ishan Baniya <ibaniya98@gmail.com>
2021-03-19 12:02:38 -0400

    Refactor existing navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,40 @@ import React from 'react';
 import { Flex, Link } from '@chakra-ui/react';
 import { Link as GatsbyLink } from 'gatsby';
 
+interface NavLinkProps {
+  link: string;
+  text: string;
+}
+
+function NavLink(props: NavLinkProps): JSX.Element {
+  return (
+    <Link
+      _hover={{
+        textDecoration: 'underline',
+        textDecorationColor: '#65BF73',
+        textDecorationThickness: '3px',
+      }}
+      fontFamily="Avenir"
+      fontWeight="850"
+      fontSize="16px"
+      as={GatsbyLink}
+      to={props.link}
+    >
+      {props.text}
+    </Link>
+  );
+}
+
 function Navigation(): JSX.Element {
+  const navigationItems: { [link: string]: string } = {
+    '/': 'HOME',
+    '/about': 'ABOUT',
+    '/receipes/': 'RECIPES',
+    '/events-and-classes/': 'EVENTS & CLASSES',
+    '/blog/': 'BLOG',
+    '/resources/': 'RESOURCES',
+  };
+
   return (
     <Flex
       h="55px"
@@ -14,95 +47,9 @@ function Navigation(): JSX.Element {
       justifyContent="space-evenly"
       alignItems="center"
     >
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/"
-      >
-        HOME
-      </Link>
-
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/about"
-      >
-        ABOUT
-      </Link>
-
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/recipes/"
-      >
-        RECIPES
-      </Link>
-
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/events-and-classes/"
-      >
-        EVENTS & CLASSES
-      </Link>
-
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/blog/"
-      >
-        BLOG
-      </Link>
-
-      <Link
-        _hover={{
-          textDecoration: 'underline',
-          textDecorationColor: '#65BF73',
-          textDecorationThickness: '3px',
-        }}
-        fontFamily="Avenir"
-        fontWeight="850"
-        fontSize="16px"
-        as={GatsbyLink}
-        to="/resources/"
-      >
-        RESOURCES
-      </Link>
+      {Object.keys(navigationItems).map(link => {
+        return <NavLink link={link} text={navigationItems[link]} key={link} />;
+      })}
     </Flex>
   );
 }
