59c772be95095a5a282e949dd5bbea1478b8d6d5
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2021-03-21 03:42:09 -0400

    added donate section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Container, Divider, Heading, Text, Button, HStack, VStack, Image, Stack, Input} from '@chakra-ui/react';
+import { Box, Container, Divider, Heading, Text, Button, HStack, VStack, Image, Stack, Input, Center} from '@chakra-ui/react';
 import { graphql, PageProps } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
@@ -16,27 +16,24 @@ interface Props extends PageProps {
 function RootIndex(props: Props): JSX.Element {
   const [value, setValue] = React.useState("")
 
-  const siteTitle = props.data.site?.siteMetadata?.title;
-  const posts = props.data.allContentfulBlogPost.edges;
-  const [author] = props.data.allContentfulPerson.nodes;
+  // const siteTitle = props.data.site?.siteMetadata?.title;
+  // const posts = props.data.allContentfulBlogPost.edges;
+  // const [author] = props.data.allContentfulPerson.nodes;
 
   return (
     <Layout location={props.location}>
-      <Box margin={{ base: 10, md: 220 }}>
+      <Box w = {560} marginTop = {240} marginLeft = {180}>
       <Heading textStyle="heading1" color = "tangerine.500">Promoting Healthy Futures,</Heading>
       <Heading textStyle="heading1" color = "tangerine.500">One Meal At a Time</Heading>
-      <Heading fontWeight = "light" textStyle="subheading1" fontSize = "16px">Healthy Families - Healthy futures is dedicated to empowering teachers,</Heading>
-      <Heading fontWeight = "light" textStyle="subheading1" fontSize = "16px">school staff, and families of young children to embrace healthy eating habits</Heading>
-      <Heading fontWeight = "light" textStyle="subheading1" fontSize = "16px">by equipping them with the tools and educated support system they need to</Heading>
-      <Heading fontWeight = "light" textStyle="subheading1" fontSize = "16px">make healthy food choices.</Heading>
-      <Box marginTop = {31}>
+      <Heading fontWeight = "light" textStyle="subheading1" fontSize = "16px">Healthy Families - Healthy futures is dedicated to empowering teachers, school staff, and families of young children to embrace healthy eating habits by equipping them with the tools and educated support system they need to make healthy food choices.</Heading>
+      <Box marginTop = {31} marginBottom = {300}>
       <Button variant="neutral" fontSize="16px">
           Learn More
         </Button>
         </Box>
       </Box>
 
-      <Box h = {840} bg = "gray.light">
+      <Box h = {900} bg = "gray.light">
       <HStack marginBottom = {170}>
         <Box w = {500}>
         <Text textAlign = {'center'} textStyle="heading1">OUR WORK</Text> 
@@ -44,7 +41,7 @@ function RootIndex(props: Props): JSX.Element {
         <Box bg = "tangerine.500" w = "100%" p = {8}></Box>
       </HStack>
 
-      <HStack marginLeft = {157} marginRight = {158}>
+      <HStack marginLeft = "10%" marginRight = "10%">
         <Box boxShadow = "lg" w = {499} h = {468} marginRight = {127} bg = "white"> 
         <VStack> 
           <Box w = {312} h = {246}>
@@ -89,10 +86,38 @@ function RootIndex(props: Props): JSX.Element {
         </Box>
       </HStack>
 
+     <HStack>
+      <Box w = "40%" h = {480}>
+      <img src = "Chef.png"></img>
+      </Box>
+      <Box w = "60%" h = {480} bg = "tangerine.500">
+      <Box h = {155} bg = "tangerine.500"></Box>
+        <Stack direction = "row">
+        <Box marginLeft = {170} w = {183} h = {158}>
+        <img src = "square-logo.png"></img>
+        </Box>
+        <Stack direction = "column">
+        <Box w = {320} marginLeft = {55}>
+        <Heading textAlign = "left" textStyle="heading1" color = "charcoal">Help Us Grow</Heading>
+        <Text textAlign = "left" textStyle="body1">Even small contributions can help us continue our work to provide nutritional education</Text>
+        <Box marginTop = {5}>
+        <Button align = "left" variant="primary" fontSize="16px">
+          DONATE
+        </Button>
+        </Box>
+        </Box>
+        </Stack> 
+        </Stack>
+      </Box>
+      </HStack>
+    
+
+
       <Box h = {75} bg = "gray.light"></Box>
       <Box h = {200} bg = "gray.light">
         <Text color = "charcoal" textAlign = {'center'} textStyle="heading2">Sign up for our newsletter!</Text>
-        <HStack marginLeft = {513} marginRight = {493} marginTop = {30}>
+        <Stack direction = "row" marginLeft = "40%" marginTop = {30}>
+          <Center>
           <Input 
              borderColor = "black"
              borderRadius = "none"
@@ -102,34 +127,12 @@ function RootIndex(props: Props): JSX.Element {
           <Button  variant="neutral" fontSize="16px">
           Subscribe
         </Button>
-        </HStack>
+        </Center>
+        </Stack>
 
       </Box>
 
-
-
-
-
-
-
-     
-      <Container maxW="5xl" style={{ background: '#fff' }}>
-        <Helmet title={siteTitle} />
-        <Hero data={author} />
-        <Box py="10">
-          <Heading as="h3">Recent articles</Heading>
-          <Divider />
-          <ul className="article-list">
-            {posts.map(({ node }) => {
-              return (
-                <li key={node.slug}>
-                  <ArticlePreview data={node} />
-                </li>
-              );
-            })}
-          </ul>
-        </Box>
-      </Container>
+  
     </Layout>
   );
 }
@@ -157,3 +160,23 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+/** 
+<Container maxW="5xl" style={{ background: '#fff' }}>
+<Helmet title={siteTitle} />
+<Hero data={author} />
+<Box py="10">
+  <Heading as="h3">Recent articles</Heading>
+  <Divider />
+  <ul className="article-list">
+    {posts.map(({ node }) => {
+      return (
+        <li key={node.slug}>
+          <ArticlePreview data={node} />
+        </li>
+      );
+    })}
+  </ul>
+</Box>
+</Container>
+*/
