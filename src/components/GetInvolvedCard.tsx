3464f391d7245568914aa868834a53a087e30132
Edmund Xin <mxinburritos@gmail.com>
2021-04-13 14:54:12 -0700

    fix gap below image

--- src/components/GetInvolvedCard.tsx
+++ src/components/GetInvolvedCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Text, Flex, Spacer } from '@chakra-ui/react';
+import { Box, Text, Flex, Center } from '@chakra-ui/react';
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
@@ -12,12 +12,18 @@ const GetInvolvedCard = ({ data }: Props) => {
   const { title, icon, link, description } = data;
 
   const imageEl =
-    icon?.fluid != null ? <Img fluid={icon.fluid} imgStyle={{ objectFit: 'contain' }} /> : null;
+    icon?.fluid != null ? (
+      <Img
+        style={{ marginTop: '50%', transform: 'translateY(-70%)' }}
+        fluid={icon.fluid}
+        imgStyle={{ objectFit: 'contain' }}
+      />
+    ) : null;
 
   return (
     <Link to={link ?? '#'}>
       <Flex direction="column" align="center" w={{ base: '30vw', md: 'auto' }} my="auto">
-        <Box w={{ base: 150, md: 312 }} h={{ base: 100, md: 240 }} mb={30}>
+        <Box w={{ base: 150, md: 312 }} h={{ base: 100, md: 246 }} mb={{ base: 30, md: 0 }}>
           {imageEl}
         </Box>
         <Text color="charcoal" textTransform="uppercase" textStyle="heading2">
