fdeaa661ef659674649d3764038a07d46f8806b8
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2021-04-13 18:14:08 -0400

    minor

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -28,7 +28,7 @@ const CardList = ({ children }: Props) => {
         <Center
           w={{ base: '27px', md: '60px' }}
           h={{ base: '52px', md: '100px' }}
-          mr={{ base: '-15px', md: '-30px' }}
+          mr={{ base: '-18px', md: '-30px' }}
           bg="charcoal"
           cursor="pointer"
           zIndex={3}
@@ -37,7 +37,12 @@ const CardList = ({ children }: Props) => {
           <ChevronLeftIcon boxSize="24px" color="white" />
         </Center>
       )}
-      <HStack spacing="40px" ml={!hasPrev ? '30px' : ''} mr={!hasNext ? '30px' : ''}>
+      <HStack
+        alignItems="start"
+        spacing="40px"
+        ml={!hasPrev ? '30px' : ''}
+        mr={!hasNext ? '30px' : ''}
+      >
         {children
           .map((card, i) => <Box key={i}>{card}</Box>)
           .filter((_, i) => i >= startIndex && i < startIndex + numCards)}
@@ -46,7 +51,7 @@ const CardList = ({ children }: Props) => {
         <Center
           w={{ base: '27px', md: '60px' }}
           h={{ base: '52px', md: '100px' }}
-          ml={{ base: '-15px', md: '-30px' }}
+          ml={{ base: '-18px', md: '-30px' }}
           zIndex={3}
           bg="charcoal"
           cursor="pointer"
